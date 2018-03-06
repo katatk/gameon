8107e5d60ad787e567757412c0a4acaa0b2c4bc0
Kat <katatk@gmail.com>
2018-03-06 14:08:18 +1300

    added back to top button

--- astra-child/js/script.js
+++ astra-child/js/script.js
@@ -6,4 +6,24 @@ jQuery(document).ready(function($) {
   $(".card > .elementor-column-wrap > .elementor-widget-wrap").append("<span class='corner-top-right'></span>");
   $(".card > .elementor-column-wrap> .elementor-widget-wrap").append("<span class='corner-bottom-right'></span>");
 
+  // scroll to top function
+  // show button when more than 50px from top
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  $('#back-to-top').click(function() {
+    // first parameter is the property to be animated
+    // second is duration in milliseconds
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+
 });
